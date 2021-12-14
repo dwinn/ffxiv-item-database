9b8103915e66ae3277b7bfe069d7e9219dc0ea24
David Winn <dwinn@turnitin.com>
2021-12-14 22:26:41 +0000

    Initial project

---
+++ src/main/java/com/dwinn/javarestservice/dto/Item.java
@@ -0,0 +1,35 @@
+package com.dwinn.javarestservice.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * <description>.
+ *
+ * @author David Winn (dwinn@turnitin.com)
+ */
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Item {
+
+	@JsonProperty("ID")
+	private int id;
+
+	@JsonProperty("Name_en")
+	private String nameEn;
+
+	@JsonProperty("LevelEquip")
+	private int levelEquip;
+
+	@JsonProperty("LevelItem")
+	private int levelItem;
+
+	@JsonProperty("ClassJobCategory")
+	private List<ClassJob> classJobCategory;
+
+	@JsonProperty("ID")
+	private String icon;
+}
---
+++ src/test/java/com/dwinn/javarestservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwinn.javarestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
